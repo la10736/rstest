0c82baba5a2686f7d6a23f18f6835454c21f5914
michele <michele.damico@gmail.com>
2019-02-02 18:12:49 +0100

    WIP: Migrate to 2018 epoch... follow warning message

--- tests/root/mod.rs
+++ tests/root/mod.rs
@@ -4,16 +4,16 @@ use crate::prj::Project;
 use crate::utils::testname;
 
 lazy_static! {
-    static ref root_dir: TempDir = TempDir::default().permanent();
-    static ref root_project: Project = Project::new(root_dir.as_ref()).create();
+    static ref ROOT_DIR: TempDir = TempDir::default().permanent();
+    static ref ROOT_RPOJECT: Project = Project::new(ROOT_DIR.as_ref()).create();
 }
 
 pub fn prj() -> Project {
     let prj_name = testname();
 
-    root_project.workspace_add(&prj_name);
+    ROOT_RPOJECT.workspace_add(&prj_name);
 
-    Project::new(root_project.path())
+    Project::new(ROOT_RPOJECT.path())
         .name(prj_name)
         .create()
 }
