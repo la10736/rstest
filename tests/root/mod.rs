453b73de012b53ec84db8b7caad50cc0bb31d381
Michele d'Amico <michele.damico@rcslab.it>
2019-02-04 14:01:51 +0100

    Silent compile warning

--- tests/root/mod.rs
+++ tests/root/mod.rs
@@ -5,15 +5,15 @@ use crate::utils::testname;
 
 lazy_static! {
     static ref ROOT_DIR: TempDir = TempDir::default().permanent();
-    static ref ROOT_RPOJECT: Project = Project::new(ROOT_DIR.as_ref()).create();
+    static ref ROOT_PROJECT: Project = Project::new(ROOT_DIR.as_ref()).create();
 }
 
 pub fn prj() -> Project {
     let prj_name = testname();
 
-    ROOT_RPOJECT.workspace_add(&prj_name);
+    ROOT_PROJECT.workspace_add(&prj_name);
 
-    Project::new(ROOT_RPOJECT.path())
+    Project::new(ROOT_PROJECT.path())
         .name(prj_name)
         .create()
 }
