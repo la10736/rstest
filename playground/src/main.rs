7c7211e5d6575ab4c43cd2e35370e94c5167c8b1
Michele d'Amico <michele.damico@rcslab.it>
2019-05-15 14:43:51 +0200

    Simple example to evolve in a complex one from single test that use a fixture to a complete one that use injection and parametrize.

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -1,16 +1,72 @@
 use rstest::*;
+use lazy_static::lazy_static;
+use std::path::{PathBuf, Path};
+use std::str::FromStr;
+
+#[derive(PartialEq, Debug, Default)]
+struct Data {}
+impl FromStr for Data {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        if s.is_empty() {
+            Ok(Default::default())
+        } else {
+            Err("Not Implemented Yet".to_owned())
+        }
+    }
+}
+
+type SyncGuard = std::sync::MutexGuard<'static, ()>;
+#[fixture]
+fn synchronize() -> SyncGuard {
+    use std::sync::Mutex;
+    lazy_static!{
+        static ref MUTEX : Mutex<()> = Mutex::new(());
+    }
+    MUTEX.lock().unwrap()
+}
+
+struct TempFile(PathBuf);
+impl AsRef<Path> for TempFile {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
+impl Drop for TempFile {
+    fn drop(&mut self) {
+        std::fs::remove_file(&self.0).unwrap();
+    }
+}
 
 #[fixture]
-pub fn i() -> u32 {
-    42
+#[allow(unused_variables)]
+fn empty(synchronize: SyncGuard) -> TempFile {
+    use std::fs::File;
+    for n in 0.. {
+        let path = format!("empty_{}", n).into();
+        if File::open(&path).is_err() {
+            File::create(&path).expect("Cannot open");
+            return TempFile(path)
+        }
+    }
+    unreachable!()
 }
 
-#[fixture(default<impl Iterator<Item=u32>>)]
-pub fn fx<I>(i: I) -> impl Iterator<Item=I> {
-    std::iter::once(i)
+#[rstest]
+fn should_parse_empty_file<P: AsRef<Path>>(empty: P) {
+    let content = std::fs::read_to_string(&empty).unwrap();
+
+    let data: Data = content.parse().unwrap();
+
+    assert_eq!(Data::default(), data);
 }
 
-#[test]
-fn resolve() {
-    assert_eq!(42, fx::default().next().unwrap())
+#[rstest]
+fn should_parse_empty_file2<P: AsRef<Path>>(empty: P) {
+    let content = std::fs::read_to_string(&empty).unwrap();
+
+    let data: Data = content.parse().unwrap();
+
+    assert_eq!(Data::default(), data);
 }
