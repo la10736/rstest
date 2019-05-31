a973d9b96369d18b152c28dabfa02c007067bbf6
Michele d'Amico <michele.damico@rcslab.it>
2019-05-31 15:07:17 +0200

    WIP example documentation

--- playground/src/main.rs
+++ playground/src/main.rs
@@ -3,70 +3,85 @@ use lazy_static::lazy_static;
 use std::path::{PathBuf, Path};
 use std::str::FromStr;
 
-#[derive(PartialEq, Debug, Default)]
-struct Data {}
-impl FromStr for Data {
-    type Err = String;
+use rstest::*;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        if s.is_empty() {
-            Ok(Default::default())
-        } else {
-            Err("Not Implemented Yet".to_owned())
-        }
+struct Entry { name: String, age: u8 }
+trait Repository {
+    fn add(&mut self, name: &str, age: u8) -> &Entry;
+    fn entries<'a, 'b: 'a>(&'b self) -> Box<dyn Iterator<Item=&'b Entry> + 'a>;
+}
+trait Processor { fn send(&mut self, message: &str, entry: &Entry); }
+
+#[derive(Default)]
+struct Rep(Vec<Entry>);
+impl Repository for Rep {
+    fn add(&mut self, name: &str, age: u8) -> &Entry {
+        self.0.push(Entry { name: name.to_owned() , age });
+        self.0.last().unwrap()
+    }
+
+    fn entries<'a, 'b: 'a>(&'b self) -> Box<dyn Iterator<Item=&'b Entry> + 'a> {
+        Box::new(self.0.iter())
     }
 }
 
-type SyncGuard = std::sync::MutexGuard<'static, ()>;
+struct RepositoryProcessor<R, P>
+    where R: Repository,
+        P: Processor
+{
+    repository: R,
+    processor: P
+}
+
+impl<R, P> RepositoryProcessor<R, P>
+    where R: Repository,
+          P: Processor
+{
+    pub fn new(repository: R, processor: P) -> Self {
+         RepositoryProcessor { repository, processor }
+    }
+
+    pub fn send_all(&mut self, message: &str){
+        unimplemented!()
+    }
+}
+
 #[fixture]
-fn synchronize() -> SyncGuard {
-    use std::sync::Mutex;
-    lazy_static!{
-        static ref MUTEX : Mutex<()> = Mutex::new(());
-    }
-    MUTEX.lock().unwrap()
+fn empty_repository() -> Rep {
+    Default::default()
 }
 
-struct TempFile(PathBuf);
-impl AsRef<Path> for TempFile {
-    fn as_ref(&self) -> &Path {
-        &self.0
-    }
+#[fixture]
+fn alice_and_bob(mut empty_repository: impl Repository) -> impl Repository {
+     empty_repository.add("Bob", 21);
+     empty_repository.add("Alice", 22);
+     empty_repository
 }
-impl Drop for TempFile {
-    fn drop(&mut self) {
-        std::fs::remove_file(&self.0).unwrap();
+
+#[derive(Default)]
+struct FakeProcessor{
+    output: String
+}
+
+impl Processor for FakeProcessor {
+    fn send(&mut self, message: &str, entry: &Entry) {
+        unimplemented!()
     }
 }
 
 #[fixture]
-#[allow(unused_variables)]
-fn empty(synchronize: SyncGuard) -> TempFile {
-    use std::fs::File;
-    for n in 0.. {
-        let path = format!("empty_{}", n).into();
-        if File::open(&path).is_err() {
-            File::create(&path).expect("Cannot open");
-            return TempFile(path)
-        }
-    }
-    unreachable!()
+fn string_processor() -> FakeProcessor {
+    Default::default()
 }
+
 
 #[rstest]
-fn should_parse_empty_file<P: AsRef<Path>>(empty: P) {
-    let content = std::fs::read_to_string(&empty).unwrap();
+fn should_process_two_users(alice_and_bob: impl Repository, string_processor: FakeProcessor) {
+    let mut processor = RepositoryProcessor::new(alice_and_bob, string_processor);
 
-    let data: Data = content.parse().unwrap();
+    processor.send_all("Good Morning");
 
-    assert_eq!(Data::default(), data);
-}
-
-#[rstest]
-fn should_parse_empty_file2<P: AsRef<Path>>(empty: P) {
-    let content = std::fs::read_to_string(&empty).unwrap();
-
-    let data: Data = content.parse().unwrap();
-
-    assert_eq!(Data::default(), data);
+    assert_eq!(2, processor.processor.output.matches("Good Morning").count());
+    assert!(processor.processor.output.contains("Bob"));
+    assert!(processor.processor.output.contains("Alice"));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -64,11 +64,11 @@
 //!
 //! #[rstest]
 //! fn should_process_two_users(alice_and_bob: impl Repository, string_processor: FakeProcessor) {
-//!     processor.send_all("Good Morning");
+//!     string_processor.send_all("Good Morning");
 //!
-//!     assert_eq!(2, processor.output.find("Good Morning").count());
-//!     assert!(processor.output.contains("Bob"));
-//!     assert!(processor.output.contains("Alice"));
+//!     assert_eq!(2, string_processor.output.find("Good Morning").count());
+//!     assert!(string_processor.output.contains("Bob"));
+//!     assert!(string_processor.output.contains("Alice"));
 //! }
 //! ```
 //!
@@ -78,105 +78,10 @@
 //! function will resolve each argument name by call the fixture function. Fixture functions should
 //! be marked by `[fixture]` attribute.
 //!
-//! Lets see a concrete example:
-//!
-//! You have to write a file parser. The first test is:
+//! We can recall previous example and write all details and add one other test case.
 //!
 //! ```
-//! # use std::path::{PathBuf, Path};
-//! # #[derive(Default, Debug, PartialEq)]
-//! # struct Matrix { rows: usize, cols: usize, data: Vec<i32> }
-//! # impl<P: AsRef<Path>> From<P> for Matrix { fn from(_: P) -> Self { Default::default() } }
-//! # fn create_empty_file() -> PathBuf { "".into() }
-//!
-//! # //noinspection ALL
-//! //#[test] <--- this is intentional... we're just discovering test interface
-//! fn should_parse_empty_file() {
-//!     let empty = create_empty_file();
-//!
-//!     let matrix: Matrix = Matrix::from(empty);
-//!
-//!     assert_eq!(matrix, Default::default())
-//! }
 //! ```
-//!
-//! Ok, `create_empty_file()` should do a lot of job and can be reused in other tests and fixtures:
-//! it's a good candidate to be a fixture. Let's rewrite previous example by use `rstest`: we'll use
-//! `empty` for fixture instead of `create_empty_file()` because when we use as fixture will be an
-//! object instead an action.
-//!
-//! ```
-//! # use std::path::{PathBuf, Path};
-//! # #[derive(Default, Debug, PartialEq)]
-//! # struct Matrix { rows: usize, cols: usize, data: Vec<i32> }
-//! # impl<P: AsRef<Path>> From<P> for Matrix { fn from(_: P) -> Self { Default::default() } }
-//! use rstest::*;
-//!
-//! #[fixture]
-//! fn empty() -> PathBuf {
-//!     let path = "empty_file".into();
-//!     std::fs::File::create(&path).expect("Cannot open");
-//!     path
-//! }
-//!
-//! #[rstest]
-//! fn should_parse_empty_file(empty: PathBuf) {
-//!     let matrix: Matrix = Matrix::from(empty);
-//!
-//!     assert_eq!(matrix, Default::default())
-//! }
-//! ```
-//!
-//! Now our `Matrix`'s `From` trait is a little bit more generic and can take every struct that
-//! implement `AsRef<Path>` like `PathBuf` do. So we can generalize our test too:
-//!
-//! ```
-//! # use std::path::{PathBuf, Path};
-//! # #[derive(Default, Debug, PartialEq)]
-//! # struct Matrix { rows: usize, cols: usize, data: Vec<i32> }
-//! # impl<P: AsRef<Path>> From<P> for Matrix { fn from(_: P) -> Self { Default::default() } }
-//! # use rstest::*;
-//!
-//! #[rstest]
-//! fn should_parse_empty_file<P>(empty: P)
-//!     where P: AsRef<Path>
-//! {
-//!     let matrix: Matrix = Matrix::from(empty);
-//!
-//!     assert_eq!(matrix, Default::default())
-//! }
-//! ```
-//!
-//! Now our test is neat and clear but we have a big issue to fix: we cannot leave `"empty_file"`
-//! on our disk, we must remove it we are done! That is a job for Rust ownership!!
-//!
-//! We should just wrap `PathBuf` in out `TempFile` struct and implement both `Drop` and
-//! `AsRef<Path>` traits. We'll use `Drop` to delete it.
-//!
-//! ```
-//! use rstest::*;
-//! use std::path::{PathBuf, Path};
-//! struct TempFile(PathBuf);
-//!
-//! impl Drop for TempFile {fn drop(&mut self) {
-//!         std::fs::remove_file(&self.0).unwrap();
-//!     }
-//! }
-//!
-//! impl AsRef<Path> for TempFile {
-//!    fn as_ref(&self) -> &Path {
-//!        &self.0
-//!    }
-//! }
-//!
-//! #[fixture]
-//! fn empty() -> TempFile {
-//!     let path = "empty_file".into();
-//!     std::fs::File::create(&path).expect("Cannot open");
-//!     TempFile(path)
-//! }
-//! ```
-//!
 //!
 
 
