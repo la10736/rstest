fadae59f7dfdf4d0ea6d824187d9c9301513cdbf
michele <michele.damico@gmail.com>
2019-12-08 17:35:26 +0100

    Self publishing docs

---
+++ docs/head/search-index.js
@@ -0,0 +1,4 @@
+var N=null,E="",T="t",U="u",searchIndex={};
+var R=["rstest"];
+searchIndex["rstest"]={"doc":"This crate will help you to write simpler tests by…","i":[[23,"fixture",R[0],"Define a fixture that you can use in all `rstest`'s test…",N,N],[23,R[0],E,"`#[rstest]` is the attribute that you should use for your…",N,N],[23,"rstest_parametrize",E,"Write table-based tests: you must indicate the arguments…",N,N],[23,"rstest_matrix",E,"Write matrix-based tests: you must indicate arguments and…",N,N]],"p":[]};
+addSearchOptions(searchIndex);initSearch(searchIndex);
---
+++ docs/head/source-files.js
@@ -0,0 +1,3 @@
+var N = null;var sourcesIndex = {};
+sourcesIndex["rstest"] = {"name":"","dirs":[{"name":"parse","files":["fixture.rs","macros.rs","mod.rs","rstest.rs","testcase.rs","vlist.rs"]},{"name":"render","files":["fixture.rs","mod.rs","wrapper.rs"]}],"files":["error.rs","lib.rs","refident.rs","resolver.rs","utils.rs"]};
+createSourceSidebar();
