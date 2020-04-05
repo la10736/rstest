d81c8bf79dafed5b464925f1c68a68d119413fdf
michele <michele.damico@gmail.com>
2020-04-05 22:41:15 +0200

    #73  Forget ) in docs

--- src/lib.rs
+++ src/lib.rs
@@ -618,8 +618,8 @@ pub fn fixture(
 /// #[rstest(expected, a, b,
 ///     case(5, 2, 3),
 ///     #[should_panic]
-///     case(42, 40, 1),
-///  ]
+///     case(42, 40, 1)
+///  )]
 /// async fn my_async_test(expected: u32, a: u32, b: u32) {
 ///     assert_eq!(expected, async_sum(a, b).await);
 /// }
