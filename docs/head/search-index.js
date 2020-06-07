ded847fb829a93d95d440c5cf5ec40d081822668
GitHub Action <action@github.com>
2020-06-07 10:28:51 +0000

    Update docs

--- docs/head/search-index.js
+++ docs/head/search-index.js
@@ -1,3 +1,4 @@
-var searchIndex={};
-searchIndex["rstest"] = {"doc":"This crate will help you to write simpler tests by…","i":[[23,"fixture","rstest","Define a fixture that you can use in all `rstest`'s test…",null,null],[23,"rstest","","The attribute that you should use for your tests. Your…",null,null]],"p":[]};
+var searchIndex = JSON.parse('{\
+"rstest":{"doc":"This crate will help you to write simpler tests by…","i":[[23,"fixture","rstest","Define a fixture that you can use in all `rstest`\'s test…",null,null],[23,"rstest","","The attribute that you should use for your tests. Your…",null,null]],"p":[]}\
+}');
 addSearchOptions(searchIndex);initSearch(searchIndex);
