ab9215044369baec5190b9065f93b4a81a0fafdd
la10736 <michele.damico@gmail.com>
2024-05-19 11:58:52 +0200

    End to end tests

--- rstest/tests/resources/rstest/rename.rs
+++ rstest/tests/resources/rstest/rename.rs
@@ -5,8 +5,22 @@ fn very_long_and_boring_name(#[default(42)] inject: u32) -> u32 {
     inject
 }
 
+mod sub_module {
+    use super::*;
+
+    #[fixture]
+    pub fn mod_fixture() -> u32 {
+        42
+    }
+}
+
 #[rstest(very_long_and_boring_name as foo)]
 fn compact(foo: u32) {
+    assert!(42 == foo);
+}
+
+#[rstest(sub_module::mod_fixture as foo)]
+fn compact_mod(foo: u32) {
     assert!(42 == foo);
 }
 
@@ -17,6 +31,11 @@ fn compact_injected(foo: u32) {
 
 #[rstest]
 fn attribute(#[from(very_long_and_boring_name)] foo: u32) {
+    assert!(42 == foo);
+}
+
+#[rstest]
+fn attribute_mod(#[from(sub_module::mod_fixture)] foo: u32) {
     assert!(42 == foo);
 }
 
